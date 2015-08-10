e1fecc6780872f9e43add7ba0e2bcc8c78d85410
Robert Ross <rross@digitalocean.com>
2015-08-10 00:25:04 -0400

    JK, accounts is dumb.

--- app/models/account.rb
+++
@@ -1,4 +0,0 @@
-class Account < ActiveRecord::Base
-  validates :owner_email, presence: true
-  validates :name, presence: true
-end
--- spec/factories/accounts.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :account do
-    owner_email "MyString"
-name "MyString"
-usage "MyText"
-  end
-
-end
